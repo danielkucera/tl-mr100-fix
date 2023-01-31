1325c84c3f6e6857522eeb9fe4d74ec044c40c2e
Daniel Kucera <daniel.kucera@gmail.com>
2023-01-31 15:05:33 +0100

    generate files for compare

--- compare.sh
+++ compare.sh
@@ -1,1 +1,8 @@
-meld <( dd if=$1 bs=1 count=$((0x20000)) | hd ) <( dd if=$1 bs=1 skip=$((0x20000)) count=$((0x20000)) | hd ) <( dd if=$2 bs=1 skip=$((0x20000)) count=$((0x20000)) | hd )
+FILE=$1
+FILE2=$2
+
+dd if=$FILE bs=1 count=$((0x20000)) | hd > $FILE.hd1
+dd if=$FILE bs=1 skip=$((0x20000)) count=$((0x20000)) | hd > $FILE.hd2
+dd if=$FILE2 bs=1 skip=$((0x20000)) count=$((0x20000)) | hd > $FILE2.hd2
+meld $FILE.hd1 $FILE.hd2 $FILE2.hd2
+#meld $FILE.hd1 $FILE.hd2
